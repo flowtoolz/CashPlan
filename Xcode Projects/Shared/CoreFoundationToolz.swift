d87dbf65c7ab7c102298e86b4c32c31139d1c07d
flowtoolz <contact@flowtoolz.com>
2021-08-22 13:19:41 +0200

    Turn unix epoch timestamp into function

--- Xcode Projects/Shared/CoreFoundationToolz.swift
+++ Xcode Projects/Shared/CoreFoundationToolz.swift
@@ -4,4 +4,6 @@ import CoreFoundation
  In case a model needs to get the current time but should neither import Foundation nor rely on some injected "time provider", we could allow the model layer to use CoreFoundation, which is only a small subset of Foundation and available on Linux.
  See https://github.com/apple/swift-corelibs-foundation
  */
-let unixEpochTimestamp = CFAbsoluteTimeGetCurrent() + kCFAbsoluteTimeIntervalSince1970
+func unixEpochTimestamp() -> CFAbsoluteTime {
+    CFAbsoluteTimeGetCurrent() + kCFAbsoluteTimeIntervalSince1970
+}
