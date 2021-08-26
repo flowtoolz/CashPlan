86c762504e7919d18c048f497451ad1b8e47d04a
flowtoolz <contact@flowtoolz.com>
2021-08-26 07:51:37 +0200

    Create a new universal project (code signing still lacking)

---
+++ Xcode Project/Trademate/Shared/TrademateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrademateApp.swift
+//  Shared
+//
+//  Created by seb on 26.08.21.
+//
+
+import SwiftUI
+
+@main
+struct TrademateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
