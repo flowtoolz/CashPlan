f82cd3603ba019c548811b9557b8cfc151939997
flowtoolz <contact@flowtoolz.com>
2021-08-27 19:19:32 +0200

    Remove old Xcode project

--- Xcode Project/Trademate/Code/iOS/CoreFoundationToolz.swift
+++
@@ -1,9 +0,0 @@
-import CoreFoundation
-
-/**
- In case a model needs to get the current time but should neither import Foundation nor rely on some injected "time provider", we could allow the model layer to use CoreFoundation, which is only a small subset of Foundation and available on Linux.
- See https://github.com/apple/swift-corelibs-foundation
- */
-func unixEpochTimestamp() -> CFAbsoluteTime {
-    CFAbsoluteTimeGetCurrent() + kCFAbsoluteTimeIntervalSince1970
-}
