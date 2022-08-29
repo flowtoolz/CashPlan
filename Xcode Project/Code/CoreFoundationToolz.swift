4da34b63961475ad6d26ffc28ea97369d4d58e3a
Sebastian Fichtner <contact@flowtoolz.com>
2022-08-29 17:01:56 +0200

    Document how to download historical data via yahoo URL request

--- Xcode Project/Code/CoreFoundationToolz.swift
+++ Xcode Project/Code/CoreFoundationToolz.swift
@@ -1,7 +1,9 @@
 import CoreFoundation
 
 /**
- In case a model needs to get the current time but should neither import Foundation nor rely on some injected "time provider", we could allow the model layer to use CoreFoundation, which is only a small subset of Foundation and available on Linux.
+ For simple yahoo requests, we need the current time as unix epoch timestamp to demarcate the time interval: https://query1.finance.yahoo.com/v7/finance/download/AAPL?period1=1630227368&period2=1661763368&interval=1d&events=history&includeAdjustedClose=true
+ 
+ Generall: In case a model needs to get the current time but should neither import Foundation nor rely on some injected "time provider", we could allow the model layer to use CoreFoundation, which is only a small subset of Foundation and available on Linux.
  See https://github.com/apple/swift-corelibs-foundation
  */
 func unixEpochTimestamp() -> CFAbsoluteTime {
