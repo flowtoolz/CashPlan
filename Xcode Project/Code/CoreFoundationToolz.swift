21d3efa872eb06bf7d43f413cb3eb7208c3d8239
Sebastian Fichtner <sebastian.fichtner@zuehlke.com>
2024-03-02 12:08:19 +0100

    Edit comment

--- Xcode Project/Code/CoreFoundationToolz.swift
+++ Xcode Project/Code/CoreFoundationToolz.swift
@@ -3,7 +3,7 @@ import CoreFoundation
 /**
  For simple yahoo requests, we need the current time as unix epoch timestamp to demarcate the time interval: https://query1.finance.yahoo.com/v7/finance/download/AAPL?period1=1630227368&period2=1661763368&interval=1d&events=history&includeAdjustedClose=true
  
- Generall: In case a model needs to get the current time but should neither import Foundation nor rely on some injected "time provider", we could allow the model layer to use CoreFoundation, which is only a small subset of Foundation and available on Linux.
+ Generally: In case a model needs to get the current time but should neither import Foundation nor rely on some injected "time provider", we could allow the model layer to use CoreFoundation, which is only a small subset of Foundation and available on Linux.
  See https://github.com/apple/swift-corelibs-foundation
  */
 func unixEpochTimestamp() -> CFAbsoluteTime {
