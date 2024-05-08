3f72478c6d36a0410152154a14c83df1be3fbaf6
Sebastian Fichtner <sebastian.fichtner@zuehlke.com>
2024-05-08 18:15:17 +0200

    Finish persisting future vision input

--- Xcode Project/Code/iOS/Future Vision/FutureVision.swift
+++ Xcode Project/Code/iOS/Future Vision/FutureVision.swift
@@ -1,20 +1,23 @@
+import FoundationToolz
 import Foundation
 import Combine
 
 class FutureVision: ObservableObject {
+    
+    // MARK: - Singleton Instance
+    
     static let shared = FutureVision()
+    
     private init() {
-        observation = Portfolio.shared.$balanceNumericalValue.sink { [weak self] newValue in
-            self?.input.startCash = newValue
-        }
+        updateStartCashWheneverPortfolioBalanceChanges()
     }
     
-    private var observation: AnyCancellable?
+    // MARK: - Calculate Output from Input (Whenever ouput is accessed)
     
     var output: Output { Self.output(from: input) }
     
     // TODO: write unit tests for simple base cases to ensure there is no "off by one" issue going on
-    static func output(from input: Input) -> Output {
+    private static func output(from input: Input) -> Output {
         let months = Int(input.investmentAssumption.years * 12)
         let growthPerMonth = pow(input.investmentAssumption.annualGrowthFactor, 1.0 / 12.0)
         
@@ -31,23 +34,72 @@ class FutureVision: ObservableObject {
                      cashflow: resultingCashFlowPerMonth)
     }
     
+    struct Output {
+        let cash: Double
+        let cashflow: Double
+    }
+    
+    // MARK: - Observable Input (Input = Start Cash + Investment Assumption)
+    
+    private func updateStartCashWheneverPortfolioBalanceChanges() {
+        portfolioBalanceObservation = Portfolio.shared.$balanceNumericalValue.sink { [weak self] newBalance in
+            self?.input.startCash = newBalance
+        }
+    }
+    
+    private var portfolioBalanceObservation: AnyCancellable?
+    
     @Published var input = Input(startCash: Portfolio.shared.balanceNumericalValue,
-                                 investmentAssumption: .init(monthlyInvestment: 1000, 
-                                                             annualReturnPercent: 8.5,
-                                                             years: 5))
+                                 investmentAssumption: persistedInvestmentAssumption) {
+        didSet {
+            // when the input changes, we want to persist the investmentAssumption
+            Self.persistedInvestmentAssumption = input.investmentAssumption
+        }
+    }
     
     struct Input {
         var startCash: Double
         var investmentAssumption: InvestmentAssumption
     }
     
-    struct Output {
-        let cash: Double
-        let cashflow: Double
+    // MARK: - Persisted Investment Assumption
+    
+    private static var persistedInvestmentAssumption: InvestmentAssumption {
+        get {
+            guard let data = UserDefaults.standard.data(forKey: Self.investmentAssumptionUserDefaultsKey),
+               let loadedIA = try? InvestmentAssumption(jsonData: data) else {
+                return save(.default)
+            }
+            
+            return loadedIA
+        }
+        
+        set {
+            save(newValue)
+        }
     }
+    
+    @discardableResult
+    private static func save(_ investmentAssumption: InvestmentAssumption) -> InvestmentAssumption {
+        if let data = investmentAssumption.encode() {
+            UserDefaults.standard.set(data, forKey: investmentAssumptionUserDefaultsKey)
+        } else {
+            print("Oh no 😱")
+        }
+        
+        return investmentAssumption
+    }
+    
+    private static let investmentAssumptionUserDefaultsKey = "InvestmentAssumptionUserDefaultsKey"
 }
 
-struct InvestmentAssumption {
+struct InvestmentAssumption: Codable {
+    static var `default`: InvestmentAssumption {
+        .init(monthlyInvestment: 1000,
+              annualReturnPercent: 8.5,
+              years: 5)
+    }
+    
     var annualGrowthFactor: Double { 1.0 + annualProfitFactor }
     var monthProfitFactor: Double { annualProfitFactor / 12.0 }
     var annualProfitFactor: Double { annualReturnPercent / 100.0 }
