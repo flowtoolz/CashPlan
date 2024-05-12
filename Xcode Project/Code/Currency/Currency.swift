40b337fd2f841e83b3162b3412c97546bd2f7826
Sebastian Fichtner <sebastian.fichtner@zuehlke.com>
2024-05-12 20:18:38 +0200

    Update currency exchange rates

--- Xcode Project/Code/Currency/Currency.swift
+++ Xcode Project/Code/Currency/Currency.swift
@@ -18,32 +18,32 @@ struct Currency: Equatable, Identifiable, Codable {
                                    name: "US Dollar")
     
     static let euro = Currency(code: "EUR",
-                               dollarPrice: 1.19,
+                               dollarPrice: 1.0787,
                                symbol: "€",
                                name: "Euro")
     
     static let swissFranc = Currency(code: "CHF",
-                                     dollarPrice: 1.10,
+                                     dollarPrice: 1.0988,
                                      symbol: "CHF",
                                      name: "Swiss Franc")
     
     static let britishPound = Currency(code: "GBP",
-                                       dollarPrice: 1.39,
+                                       dollarPrice: 1.2475,
                                        symbol: "£",
                                        name: "British Pound")
     
     static let bitCoin = Currency(code: "BTC",
-                                  dollarPrice: 41579.80,
+                                  dollarPrice: 61569.8,
                                   symbol: "₿",
                                   name: "Bitcoin")
     
     static let canadianDollar = Currency(code: "CAD",
-                                         dollarPrice: 0.8,
+                                         dollarPrice: 0.7311,
                                          symbol: "C$",
                                          name: "Canadian Dollar")
     
     static let australianDollar = Currency(code: "AUD",
-                                           dollarPrice: 0.73,
+                                           dollarPrice: 0.6622,
                                            symbol: "A$",
                                            name: "Australian Dollar")
     
