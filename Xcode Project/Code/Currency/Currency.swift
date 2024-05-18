a0ad8827fb735c236ea12f8b33426f4caef8db9c
Sebastian Fichtner <sebastian.fichtner@zuehlke.com>
2024-05-18 14:33:12 +0200

    Update currency exchange rates

--- Xcode Project/Code/Currency/Currency.swift
+++ Xcode Project/Code/Currency/Currency.swift
@@ -18,32 +18,32 @@ struct Currency: Equatable, Identifiable, Codable {
                                    name: "US Dollar")
     
     static let euro = Currency(code: "EUR",
-                               dollarPrice: 1.0787,
+                               dollarPrice: 1.0894, // https://www.google.com/finance/quote/EUR-USD
                                symbol: "€",
                                name: "Euro")
     
     static let swissFranc = Currency(code: "CHF",
-                                     dollarPrice: 1.0988,
+                                     dollarPrice: 1.1001, // https://www.google.com/finance/quote/CHF-USD
                                      symbol: "CHF",
                                      name: "Swiss Franc")
     
     static let britishPound = Currency(code: "GBP",
-                                       dollarPrice: 1.2475,
+                                       dollarPrice: 1.2705, // https://www.google.com/finance/quote/GBP-USD
                                        symbol: "£",
                                        name: "British Pound")
     
     static let bitCoin = Currency(code: "BTC",
-                                  dollarPrice: 61569.8,
+                                  dollarPrice: 67194.1, // https://www.google.com/finance/quote/BTC-USD
                                   symbol: "₿",
                                   name: "Bitcoin")
     
     static let canadianDollar = Currency(code: "CAD",
-                                         dollarPrice: 0.7311,
+                                         dollarPrice: 0.7343, // https://www.google.com/finance/quote/CAD-USD
                                          symbol: "C$",
                                          name: "Canadian Dollar")
     
     static let australianDollar = Currency(code: "AUD",
-                                           dollarPrice: 0.6622,
+                                           dollarPrice: 0.6701, // https://www.google.com/finance/quote/AUD-USD
                                            symbol: "A$",
                                            name: "Australian Dollar")
     
